b0035977b16e17fa2c31f74e04be1810882872f4
Luis Pablo <luispablo@MacBook-Air-de-Luis.local>
2022-12-27 08:05:09 +0100

    FEAT: Adding form mode, and functions to get the values and keept them in a variable

--- src/vistas/home/registro.js
+++ src/vistas/home/registro.js
@@ -1,12 +1,38 @@
 import {html, css , LitElement} from 'lit-element'
 
 class Registro extends LitElement {
+    static get properties(){
+        return {
+            nombre: {type: String},
+            apellidos: {type: String},
+            edad: {type: Number}
+        }   
+
+    }
+
+    constructor(){
+        super();
+        this.nombre = this.apellidos = this.edad = "";
+    }
+
+    manejador(nombre){
+        return (event)=>{
+            this[nombre] = event.target.value;
+                    }
+    }
+
+    guardar(){
+       let detail = {nombre:this.nombre}
+   }
+
+    
     render(){
         return html`
            <form>
-                <label >Nombre <input/> </label>
-                <label >Apellido  <input/> </label>
-                <label >Edad <input/> </label>
+                <label >Nombre <input @input='${this.manejador('nombre')}' .value='${this.nombre}' /> </label>
+                <label >Apellido  <input @input='${this.manejador('apellidos')}' .value='${this.apellidos}' /> </label>
+                <label >Edad <input @input='${this.manejador('edad')}' .value='${this.edad}' /> </label>
+                <button type="button" >Guardar</button>
             
            </form>
         `;
